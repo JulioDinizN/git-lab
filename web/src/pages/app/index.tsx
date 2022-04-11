cecb988a2046dfadb0ef2bb99733ac85e0a7a72a
JulioDinizN <juliocesardiniznogueira@gmail.com>
2022-04-11 19:47:43 -0300

    Aula - 04

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,15 +1,27 @@
+import { gql, useQuery } from '@apollo/client';
 import {
   getAccessToken,
   useUser,
   withPageAuthRequired,
 } from '@auth0/nextjs-auth0';
+import {
+  useGetProductsQuery,
+  useMeQuery,
+} from '../../graphql/generated/graphql';
+import {
+  getServerPageGetProducts,
+  ssrGetProducts,
+} from '../../graphql/generated/page';
+import { withApollo } from '../../lib/withApollo';
 
-export default function Home() {
+function Home({ data }) {
   const { user } = useUser();
+  const { data: me } = useMeQuery();
 
   return (
-    <div>
+    <div className="text-violet-500">
       <h1>Hello world</h1>
+      <pre>{JSON.stringify(me, null, 2)}</pre>
       <pre>{JSON.stringify(user, null, 2)}</pre>
       <a href="/api/auth/logout">Logout</a>
     </div>
@@ -17,11 +29,13 @@ export default function Home() {
 }
 
 export const getServerSideProps = withPageAuthRequired({
-  getServerSideProps: async ({ res, req }) => {
-    console.log(getAccessToken(req, res));
+  getServerSideProps: async (ctx) => {
+    // getServerPageGetProducts({}, ctx);
 
     return {
       props: {},
     };
   },
 });
+
+export default withApollo(ssrGetProducts.withPage()(Home));
